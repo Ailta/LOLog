b3f668294c2719276cb919595702dce867dada66
Karel Kříž <KrizK21@spstrutnov.cz>
2024-03-05 15:01:03 +0100

    save

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,23 +2,33 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const [message, setMessage] = useState('');
+	function OnClick() {
+        setMessage(getMessage());
+        // uložit input text do databáze      
+    }
+	
+	return (
+		<div className="App">
+			<header className="App-header">
+				<button OnClick={OnClick}>You got mail.</button>
+				<p>{message}</p>
+			</header>
+		</div>
+	);
+}
 
-  useEffect(() => {
-    // Fetch data from the server when the component mounts
-    fetch('/api/hello')
-      .then((response) => response.json())
-	  .then((data) => setMessage(data.message))
-      .catch((error) => console.error('Error fetching data:', error));
-  }, []);
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>{message}</p>
-      </header>
-    </div>
-  );
+function getMessage() {
+	const [message, setMessage] = useState('');
+	
+	useEffect(() => {
+		// Fetch data from the server when the component mounts
+		fetch('/api/hello')
+		.then((response) => response.json())
+		.then((data) => setMessage(data.message))
+		.catch((error) => console.error('Error fetching data:', error));
+	}, []);
+	
+	return message;
 }
 
 export default App;

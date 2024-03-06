4e86cae5e3f1c6cef7607098c131953ad1c1b1d4
Karel Kříž <KrizK21@spstrutnov.cz>
2024-03-06 09:32:32 +0100

    LogIn and another one

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,74 +2,69 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const [message, setMessage] = useState('');
-  const [count, setCount] = useState(0);
-
-  function handleClick() {
-    setCount(count + 1);
-  }
-  
-  function clicked() {
-	fetch('/api/helloo')
+	const [logedIn, setLogedIn] = useState(false);
+	
+	//useEffect(() => {
+	//	// Fetch data from the server when the component mounts
+	//	fetch('/api/hello')
+	//	.then((response) => response.json())
+	//	.then((data) => setMessage(data.message))
+	//	.catch((error) => console.error('Error fetching data:', error));
+	//}, []);
+	
+	function logIn(){
+		const text = document.getElementById('text');
+		const username = document.getElementById('username').value;
+		const password = document.getElementById('password').value;
+	
+		let data = { 'username': username, 'password': password};
+	
+		fetch('/logIn', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({data})
+		})
 		.then((response) => response.json())
-		.then((data) => setMessage(data.message))
+		.then((data) => {
+			if (data.success){
+				setLogedIn(true);
+			}
+		})
 		.catch((error) => console.error('Error fetching data:', error));
-  }
-  
-  function logIn(){
-	const text = document.getElementById('text');
-	const username = document.getElementById('username').value;
-	const password = document.getElementById('password').value;
-  
-	let data = { 'username': username, 'password': password};
-  
-	fetch('/logIn', {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json'
-		},
-		body: JSON.stringify({data})
-	})
-	.then((response) => response.json())
-	.then((data) => {
-		if (data.success){
-			text.innerHTML = 'Yaaay.';
-		}
-	})
-	.catch((error) => console.error('Error fetching data:', error));
-  }
+	}
+	
+	if(logedIn){
+		return (
+			<div className="App">
+				<header className="App-header">
+				
+				</header>
+			</div>
+		);
+	} else{
+		return (
+			<div className="App">
+				<header className="App-header">
+					<LogIn onClick={logIn}/>
+				</header>
+			</div>
+		);
+	}
+}
 
-
-  useEffect(() => {
-    // Fetch data from the server when the component mounts
-    fetch('/api/hello')
-      .then((response) => response.json())
-	  .then((data) => setMessage(data.message))
-      .catch((error) => console.error('Error fetching data:', error));
-  }, []);
-
-  return (
-    <div className="App">
-      <header className="App-header">
+function LogIn({ onClick }){
+	return (
 		<div id="logInForm" style={{backgroundColor: '#202329', marginRight: '0px', paddingRight: '15px', paddingLeft: '15px', paddingBottom: '10px'}}>
 			<p style={{marginTop: '0px', marginBottom: '0px'}} id="text">LogIn</p>
 			<input type="text" id="username" name="username" placeholder="Username" required />
 			<br/>
 			<input type="password" id="password" name="password" placeholder="Password" required />
 			<br/>
-			<button onClick={logIn}>Sumbit</button>
+			<button onClick={onClick}>Sumbit</button>
 		</div>
-      </header>
-    </div>
-  );
-}
-
-function MyButton({ count, onClick }) {
-  return (
-    <button onClick={onClick}>
-      Clicked {count} times
-    </button>
-  );
+	);
 }
 
 export default App;
--- server/server.js
+++ server/server.js
@@ -8,11 +8,6 @@ app.use(express.json());
 // Serve static files from the 'build' folder
 app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-// Handle requests to the root URL
-app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
 // A simple endpoint that responds with 'Hello'
 app.get('/api/hello', (req, res) => {
 	res.json({ message: 'Hello from the server!' });
@@ -20,6 +15,11 @@ app.get('/api/hello', (req, res) => {
 
 app.get('/api/helloo', (req, res) => {
 	res.json({ message: 'Hello World!' });
+});
+
+// Handle requests to the root URL
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 // Handle form submission
